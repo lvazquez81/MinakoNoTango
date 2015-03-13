20efa3c2a104c6e9892bbbca3e5d244acb70b3bb
lvazquez81 <lvazquez81@outlook.com>
2015-03-13 00:49:21 -0700

    Added comment entity.

---
+++ MinakoNoTangoLib/Entities/CommentEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinakoNoTangoLib.Entities
+{
+    public class CommentEntity
+    {
+        public string AuthorName { get; set; }
+        public string Comment { get; set; }
+        public DateTime EditedOn { get; set; }
+        public bool ShowComment { get; set; }
+
+    }
+}

95c44dd902c5fad26b3d3fc066b17a19eded94d6
lvazquez81 <lvazquez81@outlook.com>
2015-03-29 00:42:19 -0700

    Adding Login view

---
+++ MinakoNoTango/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using MinakoNoTangoLib.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinakoNoTango.Controllers
+{
+    public class LoginController : Controller
+    {
+        private LoginModel _login;
+
+        public LoginController()
+        {
+           _login = new LoginModel();
+        }
+
+        [HttpGet]
+        public ActionResult IndexGet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IndexPost()
+        {
+            return View();
+        }
+
+
+    }
+}
